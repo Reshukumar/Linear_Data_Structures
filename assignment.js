a0cdb750838b454697fbed5528f92c8a16a3f179
Reshu Kumar <reshukumar1235@gmail.com>
2022-08-20 20:18:32 +0530

    Update assignment.js

--- assignment.js
+++ assignment.js
@@ -64,6 +64,81 @@ reverseArray(array)
 
 //ans3 ends
 
+//Q4. Write a program to print the first non-repeated character from a string?
+
+
+const NO_OF_CHARS = 256
+
+/* The function returns index of the first
+non-repeating character in a string. If
+all characters are repeating then
+returns Number.MAX_VALUE */
+function firstNonRepeating(str)
+{
+	let arr = new Array(NO_OF_CHARS)
+	for(let i=0;i<NO_OF_CHARS;i++){
+		arr[i] = [0,0];
+	}
+
+	for (let i=0;i<str.length;i++) {
+		arr[str.charCodeAt(i)][0]++;
+		arr[str.charCodeAt(i)][1]= i;
+	}
+
+	let res = Number.MAX_VALUE;
+	for (let i = 0; i < NO_OF_CHARS; i++)
+
+		// If this character occurs only
+		// once and appears before the
+		// current result, then update the
+		// result
+		if (arr[i][0] == 1)
+			res = Math.min(res, arr[i][1]);
+
+	return res;
+}
+
+/* Driver program to test above function */
+
+let str = "geeksforgeeks";
+let index = firstNonRepeating(str);
+if (index == Number.MAX_VALUE)
+	document.write("Either all characters are repeating or string is empty");
+else
+	document.write("First non-repeating character is ",str[index]);
+
+//ans 4 ends
+
+//Q5. Read about the Tower of Hanoi algorithm. Write a program to implement it.
+
+function towerOfHanoi(n, from_rod,  to_rod,  aux_rod)
+{
+        if (n == 0)
+        {
+            return;
+        }
+        towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
+        document.write("Move disk " + n + " from rod " + from_rod +
+        " to rod " + to_rod+"<br/>");
+        towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
+    }
+  
+    // Driver code
+    var n = 3; // Number of disks
+    towerOfHanoi(n, 'A', 'C', 'B');
+
+    //ans 5 ends
+
+//Q6. Read about infix, prefix, and postfix expressions. Write a program to convert postfix to prefix expression.
+
+
+
+	
+
+
+
+
+
 
 
 
